b272cbe52e57557a5c5f8fec35d8b518cab26911
Sachinikuruppu <sachiii.kuruppu@gmail.com>
2022-06-13 14:31:40 +0530

    mvc folder structure

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Welcome to cars and bids!
+        </p>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
